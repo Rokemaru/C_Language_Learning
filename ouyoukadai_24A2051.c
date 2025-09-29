63e64ac1c0bff70c6ee4119735dbaddc1fe2b66f
Rokemaru <satoikubuki@gmail.com>
2025-09-29 12:52:03 +0900

    kadai_02

---
+++ ouyoukadai_24A2051.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("私は%s_%sです。\n", "久冨木慧", "24A2051");
+
+
+    printf("学籍番号の下三桁205に100を足すと %d になります。\n", 205 + 100);
+
+
+    printf("円周率は%.2fです。\n", 3.14);
+
+    return 0;
+}
---
+++ test_0929.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("私は%s_%sです。\n", "24A2051" , "久冨木慧");
+
+    printf("%sは文字、%dは整数です。\n","A" , 123);
+
+    printf("%dは整数、%fcm は小数点を含む数です。\n", 123, 10.5);
+
+    return 0;
+}
